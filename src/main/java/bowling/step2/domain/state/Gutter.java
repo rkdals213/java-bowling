00b636f3cc38d20efec82c504bc2be6d9ff57dd1
kang <rkdals213@naver.com>
2021-09-08 19:17:37 +0900

    refactor: 변수명 수정

--- src/main/java/bowling/step2/domain/state/Gutter.java
+++ src/main/java/bowling/step2/domain/state/Gutter.java
@@ -2,7 +2,7 @@ package bowling.step2.domain.state;
 
 import bowling.step2.outputview.pitchresult.PitchPrinter;
 
-public class Gutter implements PitchStatus{
+public class Gutter implements PitchStatus {
 
     private final int count;
 
